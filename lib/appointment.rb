93865fa3a5becbb8999d98b00df9065a38c8c91b
Brittany Javalera <brittjavalera@gmail.com>
2019-07-15 04:23:50 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,10 @@
 require 'pry'
 class Appointment
-  attr_accessor :doctor, :patient, :date
+  attr_accessor :patient, :doctor, :date
 @@all = []
-def initialize(doctor, patient, date)
-  @doctor = doctor
+def initialize(patient, doctor, date)
   @patient = patient
+  @doctor = doctor
   @date = date
   @@all << self
 end
